03ebbfa4505d4469cce0f7a263631430ffacb41e
ManishV5 <bhagat.manishV5@gmail.com>
2022-12-21 14:20:39 +0530

    failing test

--- projects/calculator/calculator_test.py
+++ projects/calculator/calculator_test.py
@@ -4,7 +4,7 @@ from projects.calculator.calculator import Calculator
 class TestSum(unittest.TestCase):
     def test_sum(self):
          calculator = Calculator()
-         self.assertEqual(calculator.add(2, 2), 4)
+         self.assertEqual(calculator.add(2, 2), 5)
 
 if __name__ == '__main__':
     unittest.main()
